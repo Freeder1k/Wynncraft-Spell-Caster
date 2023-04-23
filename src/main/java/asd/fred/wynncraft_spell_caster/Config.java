5325fe5190330a7af7d77e5744f9a601fc614a75
Frederik <47461904+Freeder1k@users.noreply.github.com>
2023-04-23 13:49:32 +0200

    add config options

---
+++ src/main/java/asd/fred/wynncraft_spell_caster/Config.java
@@ -0,0 +1,95 @@
+package asd.fred.wynncraft_spell_caster;
+
+import com.google.gson.Gson;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Config implements ModMenuApi {
+    private static final Path config_dir = Paths.get(MinecraftClient.getInstance().runDirectory.getPath() + "/config");
+    private static final Path config_file = Paths.get(config_dir + "/wynncraft-spell-caster.json");
+    private static ConfigData config_data;
+
+    public static ConfigData getConfigData() {
+        if (config_data != null) return config_data;
+
+        try {
+            if (!Files.exists(config_file)) {
+                Files.createDirectories(config_dir);
+                Files.createFile(config_file);
+                config_data = new ConfigData(1, 2, false);
+                config_data.save();
+                return config_data;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            config_data = new ConfigData(1, 2, false);
+            return config_data;
+        }
+        try {
+            Gson gson = new Gson();
+            FileReader reader = new FileReader(config_file.toFile());
+            config_data = gson.fromJson(reader, ConfigData.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            config_data = new ConfigData(1, 2, false);
+        }
+        return config_data;
+    }
+
+    public static Screen createConfigScreen(Screen parent) {
+        ConfigBuilder builder = ConfigBuilder.create().setParentScreen(parent).setTitle(Text.translatable("screen.wynncraft-spell-caster.config.title"));
+
+        ConfigCategory general = builder.getOrCreateCategory(Text.translatable("General Config"));
+
+        general.addEntry(builder.entryBuilder().startIntSlider(Text.translatable("config.wynncraft-spell-caster.option.left-interval"), config_data.left_interval, 1, 20).setDefaultValue(1).setTooltip(Text.of("Set the amount of ticks to wait after a left click.")).setSaveConsumer(newValue -> config_data.left_interval = newValue).build());
+
+        general.addEntry(builder.entryBuilder().startIntSlider(Text.translatable("config.wynncraft-spell-caster.option.right-interval"), config_data.right_interval, 1, 20).setDefaultValue(2).setTooltip(Text.of("Set the amount of ticks to wait after a right click.")).setSaveConsumer(newValue -> config_data.right_interval = newValue).build());
+
+        general.addEntry(builder.entryBuilder().startBooleanToggle(Text.translatable("config.wynncraft-spell-caster.option.invert-clicks"), config_data.invert_clicks).setDefaultValue(false).setTooltip(Text.of("Set this to True if playing on archer.")).setSaveConsumer(newValue -> config_data.invert_clicks = newValue).build());
+
+        builder.setSavingRunnable(config_data::save);
+
+        return builder.build();
+    }
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return Config::createConfigScreen;
+    }
+
+    public static class ConfigData {
+        public int left_interval;
+        public int right_interval;
+        public boolean invert_clicks;
+
+        public ConfigData(int left_interval, int right_interval, boolean invert_clicks) {
+            this.left_interval = left_interval;
+            this.right_interval = right_interval;
+            this.invert_clicks = invert_clicks;
+        }
+
+        public void save() {
+            try {
+                Gson gson = new Gson();
+                FileWriter writer = new FileWriter(config_file.toFile());
+                gson.toJson(this, writer);
+                writer.flush();
+                writer.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
--- src/main/java/asd/fred/wynncraft_spell_caster/WynncraftSpellCasterClient.java
+++ src/main/java/asd/fred/wynncraft_spell_caster/WynncraftSpellCasterClient.java
@@ -15,20 +15,21 @@ import java.util.List;
 
 public class WynncraftSpellCasterClient implements ClientModInitializer, ClientLifecycleEvents.ClientStarted, ClientTickEvents.EndTick {
     private static final LinkedList<Boolean> clickQueue = new LinkedList<>();  // 0 = left click, 1 = right click
-    private static final int left_interval = 0;
-    private static final int right_interval = 1;
-    public static KeyBinding left;
-    public static KeyBinding right;
-    private static KeyBinding spell1, spell2, spell3, spell4;
+    private static final List<Boolean> spell1_clicks = Arrays.asList(true, false, true);
+    private static final List<Boolean> spell2_clicks = Arrays.asList(true, true, true);
+    private static final List<Boolean> spell3_clicks = Arrays.asList(true, false, false);
+    private static final List<Boolean> spell4_clicks = Arrays.asList(true, true, false);
+    public static KeyBinding attack_key;
+    public static KeyBinding use_key;
+    private static KeyBinding spell1_key, spell2_key, spell3_key, spell4_key;
+    private static KeyBinding config_key;
     private static int click_cooldown = 0;
     private static boolean left_clicked = false, right_clicked = false;
-    private final List<Boolean> spell1_clicks = Arrays.asList(true, false, true);
-    private final List<Boolean> spell2_clicks = Arrays.asList(true, true, true);
-    private final List<Boolean> spell3_clicks = Arrays.asList(true, false, false);
-    private final List<Boolean> spell4_clicks = Arrays.asList(true, true, false);
+    private static Config.ConfigData config_data;
 
     @Override
     public void onInitializeClient() {
+        config_data = Config.getConfigData();
         registerKeybinds();
         ClientLifecycleEvents.CLIENT_STARTED.register(this);
         ClientTickEvents.END_CLIENT_TICK.register(this);
@@ -36,47 +37,50 @@ public class WynncraftSpellCasterClient implements ClientModInitializer, ClientL
 
     @Override
     public void onClientStarted(MinecraftClient client) {
-        left = client.options.attackKey;
-        right = client.options.useKey;
+        attack_key = client.options.attackKey;
+        use_key = client.options.useKey;
     }
 
     @Override
     public void onEndTick(MinecraftClient client) {
         if (left_clicked) {
-            left.setPressed(false);
+            attack_key.setPressed(false);
             left_clicked = false;
         }
         if (right_clicked) {
-            right.setPressed(false);
+            use_key.setPressed(false);
             right_clicked = false;
         }
 
-        while (spell1.wasPressed()) {
+        if (config_key.isPressed()) {
+            config_key.setPressed(false);
+            client.setScreen(Config.createConfigScreen(client.currentScreen));
+        }
+
+        while (spell1_key.wasPressed()) {
             clickQueue.addAll(spell1_clicks);
         }
-        while (spell2.wasPressed()) {
+        while (spell2_key.wasPressed()) {
             clickQueue.addAll(spell2_clicks);
         }
-        while (spell3.wasPressed()) {
+        while (spell3_key.wasPressed()) {
             clickQueue.addAll(spell3_clicks);
         }
-        while (spell4.wasPressed()) {
+        while (spell4_key.wasPressed()) {
             clickQueue.addAll(spell4_clicks);
         }
 
-        if (click_cooldown == 0) {
-            if (!clickQueue.isEmpty()) {
-                boolean next_click = clickQueue.pop();
-                if (client.player != null) {
-                    if (next_click) {
-                        left.setPressed(true);
-                        left_clicked = true;
-                        click_cooldown += left_interval;
-                    } else {
-                        right.setPressed(true);
-                        right_clicked = true;
-                        click_cooldown += right_interval;
-                    }
+        if (click_cooldown == 0 && !clickQueue.isEmpty()) {
+            boolean next_click = clickQueue.pop();
+            if (client.player != null) {
+                if (next_click ^ config_data.invert_clicks) {
+                    attack_key.setPressed(true);
+                    left_clicked = true;
+                    click_cooldown += config_data.left_interval - 1;
+                } else {
+                    use_key.setPressed(true);
+                    right_clicked = true;
+                    click_cooldown += config_data.right_interval - 1;
                 }
             }
         }
@@ -85,9 +89,10 @@ public class WynncraftSpellCasterClient implements ClientModInitializer, ClientL
     }
 
     private void registerKeybinds() {
-        spell1 = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynncraft-spell-caster.spell.first", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.category.spell"));
-        spell2 = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynncraft-spell-caster.spell.second", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.category.spell"));
-        spell3 = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynncraft-spell-caster.spell.third", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.category.spell"));
-        spell4 = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynncraft-spell-caster.spell.fourth", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.category.spell"));
+        config_key = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynncraft-spell-caster.config", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.category.spell"));
+        spell1_key = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynncraft-spell-caster.spell.first", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.category.spell"));
+        spell2_key = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynncraft-spell-caster.spell.second", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.category.spell"));
+        spell3_key = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynncraft-spell-caster.spell.third", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.category.spell"));
+        spell4_key = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynncraft-spell-caster.spell.fourth", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.category.spell"));
     }
 }
