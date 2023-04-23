6acb45f476ffaceb8b3ed4b3d4d36ded2133103b
Frederik <47461904+Freeder1k@users.noreply.github.com>
2023-04-23 19:30:07 +0200

    remove unused stuff and add spell queue

--- src/main/java/asd/fred/wynncraft_spell_caster/Config.java
+++ src/main/java/asd/fred/wynncraft_spell_caster/Config.java
@@ -28,13 +28,13 @@ public class Config implements ModMenuApi {
             if (!Files.exists(config_file)) {
                 Files.createDirectories(config_dir);
                 Files.createFile(config_file);
-                config_data = new ConfigData(1, 3, false);
+                config_data = ConfigData.getDefault();
                 config_data.save();
                 return config_data;
             }
         } catch (IOException e) {
             e.printStackTrace();
-            config_data = new ConfigData(1, 3, false);
+            config_data = ConfigData.getDefault();
             return config_data;
         }
         try {
@@ -43,7 +43,7 @@ public class Config implements ModMenuApi {
             config_data = gson.fromJson(reader, ConfigData.class);
         } catch (IOException e) {
             e.printStackTrace();
-            config_data = new ConfigData(1, 3, false);
+            config_data = ConfigData.getDefault();
         }
         return config_data;
     }
@@ -53,9 +53,11 @@ public class Config implements ModMenuApi {
 
         ConfigCategory general = builder.getOrCreateCategory(Text.translatable("General Config"));
 
-        general.addEntry(builder.entryBuilder().startIntSlider(Text.translatable("config.wynncraft-spell-caster.option.left-interval"), config_data.left_interval, 1, 20).setDefaultValue(1).setTooltip(Text.of("Set the amount of ticks to wait after a left click.")).setSaveConsumer(newValue -> config_data.left_interval = newValue).build());
+        general.addEntry(builder.entryBuilder().startIntSlider(Text.translatable("config.wynncraft-spell-caster.option.left-interval"), config_data.left_interval, 0, 20).setDefaultValue(0).setTooltip(Text.of("Set the amount of ticks to wait after a left click.")).setSaveConsumer(newValue -> config_data.left_interval = newValue).build());
 
-        general.addEntry(builder.entryBuilder().startIntSlider(Text.translatable("config.wynncraft-spell-caster.option.right-interval"), config_data.right_interval, 1, 20).setDefaultValue(3).setTooltip(Text.of("Set the amount of ticks to wait after a right click.")).setSaveConsumer(newValue -> config_data.right_interval = newValue).build());
+        general.addEntry(builder.entryBuilder().startIntSlider(Text.translatable("config.wynncraft-spell-caster.option.right-interval"), config_data.right_interval, 0, 20).setDefaultValue(2).setTooltip(Text.of("Set the amount of ticks to wait after a right click.")).setSaveConsumer(newValue -> config_data.right_interval = newValue).build());
+
+        general.addEntry(builder.entryBuilder().startIntSlider(Text.translatable("config.wynncraft-spell-caster.option.queue-size"), config_data.queue_size, 0, 20).setDefaultValue(1).setTooltip(Text.of("Set how many extra spells the spell queue can hold.")).setSaveConsumer(newValue -> config_data.queue_size = newValue).build());
 
         general.addEntry(builder.entryBuilder().startBooleanToggle(Text.translatable("config.wynncraft-spell-caster.option.invert-clicks"), config_data.invert_clicks).setDefaultValue(false).setTooltip(Text.of("Set this to True if playing on archer.")).setSaveConsumer(newValue -> config_data.invert_clicks = newValue).build());
 
@@ -72,12 +74,18 @@ public class Config implements ModMenuApi {
     public static class ConfigData {
         public int left_interval;
         public int right_interval;
+        public int queue_size;
         public boolean invert_clicks;
 
-        public ConfigData(int left_interval, int right_interval, boolean invert_clicks) {
+        public ConfigData(int left_interval, int right_interval, int queue_size, boolean invert_clicks) {
             this.left_interval = left_interval;
             this.right_interval = right_interval;
+            this.queue_size = queue_size;
             this.invert_clicks = invert_clicks;
+        }
+
+        public static ConfigData getDefault() {
+            return new ConfigData(0, 2, 2,false);
         }
 
         public void save() {
--- src/main/java/asd/fred/wynncraft_spell_caster/WynncraftSpellCasterClient.java
+++ src/main/java/asd/fred/wynncraft_spell_caster/WynncraftSpellCasterClient.java
@@ -5,13 +5,13 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket;
 import net.minecraft.util.Hand;
 import org.lwjgl.glfw.GLFW;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,7 +22,7 @@ import java.util.List;
 public class WynncraftSpellCasterClient implements ClientModInitializer, ClientLifecycleEvents.ClientStarted, ClientTickEvents.EndTick {
 
 
-
+    public static final Logger logger = LoggerFactory.getLogger("wynncraft-spell-caster");
     private static final LinkedList<Boolean> clickQueue = new LinkedList<>();  // 0 = left click, 1 = right click
     private static final List<Boolean> spell1_clicks = Arrays.asList(true, false, true);
     private static final List<Boolean> spell2_clicks = Arrays.asList(true, true, true);
@@ -33,10 +33,8 @@ public class WynncraftSpellCasterClient implements ClientModInitializer, ClientL
     private static KeyBinding spell1_key, spell2_key, spell3_key, spell4_key;
     private static KeyBinding config_key;
     private static int click_cooldown = 0;
-    private static boolean left_clicked = false, right_clicked = false;
     private static Config.ConfigData config_data;
 
-    public static final Logger logger = LoggerFactory.getLogger("modid");
     @Override
     public void onInitializeClient() {
         config_data = Config.getConfigData();
@@ -53,86 +51,58 @@ public class WynncraftSpellCasterClient implements ClientModInitializer, ClientL
 
     @Override
     public void onEndTick(MinecraftClient client) {
-        if (left_clicked) {
-            //attack_key.setPressed(false);
-            left_clicked = false;
-        }
-        if (right_clicked) {
-            //use_key.setPressed(false);
-            right_clicked = false;
-        }
-
         if (config_key.isPressed()) {
             config_key.setPressed(false);
             client.setScreen(Config.createConfigScreen(client.currentScreen));
         }
 
-        if (spell1_key.isPressed()) {
-            clickQueue.addAll(spell1_clicks);
-            spell1_key.setPressed(false);
-        }
-        if (spell2_key.isPressed()) {
-            clickQueue.addAll(spell2_clicks);
-            spell2_key.setPressed(false);
-        }
-        if (spell3_key.isPressed()) {
-            clickQueue.addAll(spell3_clicks);
-            spell3_key.setPressed(false);
-        }
-        if (spell4_key.isPressed()) {
-            clickQueue.addAll(spell4_clicks);
-            spell4_key.setPressed(false);
-        }
+        checkSpellKey(spell1_key, spell1_clicks);
+        checkSpellKey(spell2_key, spell2_clicks);
+        checkSpellKey(spell3_key, spell3_clicks);
+        checkSpellKey(spell4_key, spell4_clicks);
 
-        if (click_cooldown == 0 && !clickQueue.isEmpty()) {
+        while (click_cooldown <= 0 && !clickQueue.isEmpty()) {
             boolean next_click = clickQueue.pop();
             if (client.player != null) {
                 if (next_click ^ config_data.invert_clicks) {
-                    //use_key.setPressed(true);
                     sendUsePacket(client);
-                    right_clicked = true;
-                    click_cooldown += config_data.right_interval - 1;
+                    click_cooldown += config_data.right_interval;
                 } else {
-                    //attack_key.setPressed(true);
                     sendAttackPacket(client);
-                    left_clicked = true;
-                    click_cooldown += config_data.left_interval - 1;
+                    click_cooldown += config_data.left_interval;
                 }
             }
         }
 
-        if (click_cooldown > 0) click_cooldown--;
+        if (click_cooldown > 0) --click_cooldown;
     }
 
     private void registerKeybinds() {
         config_key = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynncraft-spell-caster.config", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.category.spell"));
+
         spell1_key = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynncraft-spell-caster.spell.first", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.category.spell"));
         spell2_key = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynncraft-spell-caster.spell.second", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.category.spell"));
         spell3_key = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynncraft-spell-caster.spell.third", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.category.spell"));
         spell4_key = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.wynncraft-spell-caster.spell.fourth", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "key.category.spell"));
     }
 
-    private void sendAttackPacket(MinecraftClient client) {
-        try {
-            ClientPlayNetworkHandler network_handler = client.getNetworkHandler();
-            if (network_handler != null)
-                network_handler.sendPacket(new HandSwingC2SPacket(Hand.MAIN_HAND));
-            else
-                logger.error("network handler is null");
-        } catch (NullPointerException e) {
-            e.printStackTrace();
+    private void checkSpellKey(KeyBinding spell_key, List<Boolean> spell_clicks) {
+        if (spell_key.isPressed()) {
+            if (clickQueue.size() <= 3 * config_data.queue_size)
+                clickQueue.addAll(spell_clicks);
+            spell_key.setPressed(false);
         }
     }
 
+    private void sendAttackPacket(MinecraftClient client) {
+        ClientPlayNetworkHandler network_handler = client.getNetworkHandler();
+        if (network_handler != null) network_handler.sendPacket(new HandSwingC2SPacket(Hand.MAIN_HAND));
+        else logger.error("network handler is null");
+    }
+
     private void sendUsePacket(MinecraftClient client) {
-        try {
-            ClientPlayNetworkHandler network_handler = client.getNetworkHandler();
-            if (network_handler != null)
-                network_handler.sendPacket(new PlayerInteractItemC2SPacket(Hand.MAIN_HAND, 0));
-            else
-                logger.error("network handler is null");
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
+        ClientPlayNetworkHandler network_handler = client.getNetworkHandler();
+        if (network_handler != null) network_handler.sendPacket(new PlayerInteractItemC2SPacket(Hand.MAIN_HAND, 0));
+        else logger.error("network handler is null");
     }
 }
